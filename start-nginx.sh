b16f671fa605878fe3daccb7e18720577021cfaa
sarthak Gangopadhyay <ganguly.sarthak@gmail.com>
2025-12-10 23:18:40 -0500

    some changes

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "--- Environment Variables ---"
+echo "PORT is: $PORT"
+echo "---------------------------"
 envsubst '${PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 echo "--- Nginx Configuration ---"
 cat /etc/nginx/conf.d/default.conf
