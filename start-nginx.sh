5de33f5f4a2696f57ab55d4910fc7dbc8cf786f1
sarthak Gangopadhyay <ganguly.sarthak@gmail.com>
2025-12-10 23:07:32 -0500

    some changes

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 envsubst '${PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+echo "--- Nginx Configuration ---"
+cat /etc/nginx/conf.d/default.conf
+echo "--------------------------"
 nginx -g 'daemon off;'
